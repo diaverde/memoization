99b8acd23e0420ff96cc5b463addd478c203ec3e
Andrés Sánchez <diaverde77@gmail.com>
2022-01-23 23:23:09 -0500

    Description and tests included

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # memoization
-Memoization test
+Implementation of a memoization function for improving the response time of some API.
+
+This is a Javascript implementation with some mocking functions to emulate the behavior
+of a slow function which provides data upon request and some caching functions.
+
+Some test code is also provided in the end to verify that the memoization functions
+works as expected:
+<ul>
+    <li>It returns the fastest result, whether it's the cached result or the fresh result 
+        (from the slow function).</li>
+    <li>The cache is updated in either scenario.</li>
+</ul>
--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function slow_function(input) {
     return await mock_slow_function(input);
 }
 
-const cache = {};
+let cache = {};
 function memoize(slow_function) {
     return async (...args) => {
         // We define the input as the key for caching
@@ -28,17 +28,9 @@ function memoize(slow_function) {
             }).catch(err => reject(err));
             // And examine cache as well
             cache_retrieve(myKey).then(cacheResult => {
-                if (typeof cacheResult === 'undefined') {
-                    console.log('No está');
-                    slow_function(args).then(res => {
-                        resolve(res);
-                        cache_store(myKey, res);
-                    }).catch(err => reject(err));
-                } else {
-                    console.log('Ya');
+                if (typeof cacheResult !== 'undefined') {
                     resolve(cacheResult);
                 }
-                console.log(cache);
             }).catch(err => reject(err));
         });
         return result;
@@ -46,7 +38,7 @@ function memoize(slow_function) {
 };
 
 // Some Mock functions to test this code
-// Uncomment if needed
+// Uncomment if needed (from the wrapper functions above)
 
 // Mock function for cache storing
 async function mock_cache_store(key, value) {
@@ -55,8 +47,10 @@ async function mock_cache_store(key, value) {
 
 // Mock function for cache retrieval
 async function mock_cache_retrieve(key) {
+    const randomDelay = Math.floor(Math.random() * 1000);
+    console.log("Cache retrieval delay: " + randomDelay);
     let result = new Promise(function (resolve, reject) {
-        setTimeout(() => resolve(cache[key]), 1000);
+        setTimeout(() => resolve(cache[key]), randomDelay);
     });
     return result;
 }
@@ -64,14 +58,24 @@ async function mock_cache_retrieve(key) {
 // Mock function for slow_function
 const mock_slow_function = function (input) {
     const randomDelay = Math.floor(Math.random() * 2000);
-    console.log(randomDelay);
+    console.log("Slow function delay: " + randomDelay);
     let result = new Promise(function (resolve, reject) {
         setTimeout(() => resolve(input * 5), randomDelay);
     });
     return result;
 }
 
+// Some data for testing
+// Data can be preloaded in the cache and the memoized function
+// can be run several times, with different values to test that it works as expected
+cache = {1: 5, 2: 10, 4: 20, 8: 40};
 const mf = memoize(slow_function);
-mf(2).then(res => console.log(res));
+const t0 = Date.now();
+mf(1).then(res => {
+    console.log('Result: ' + res);
+    const t1 = Date.now();
+    console.log('Call to memoized function took ' + (t1 - t0) + ' milliseconds.');
+});
 
-setTimeout(() => console.log(cache), 3000);
+// Uncomment to check that cache is always updated in the end
+//setTimeout(() => console.log(cache), 3000);
